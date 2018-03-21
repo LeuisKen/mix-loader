536af76635cb3677f0e5def5b7d48eb2161b0b37
LeuisKen <weijiaxun@baidu.com>
2018-03-21 17:38:05 +0800

    feat:搭建基本开发环境

---
+++ build/dev-server.js
@@ -0,0 +1,17 @@
+/**
+ * @file 开发使用的 express 服务器
+ * @author LeuisKen <leuisken@foxmail.com>
+ */
+const path = require('path');
+const express = require('express');
+const Bundler = require('parcel-bundler');
+
+const file = path.join(__dirname, '../src/index.html');
+const options = {};
+const bundler = new Bundler(file, options);
+
+const app = express();
+
+app.use(bundler.middleware());
+
+app.listen(8080);
