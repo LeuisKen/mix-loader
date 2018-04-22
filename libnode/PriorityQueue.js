87d5843b2480636e1839de262177cbfb58fdecb3
LeuisKen <weijiaxun@baidu.com>
2018-04-22 16:24:44 +0800

    build

--- libnode/PriorityQueue.js
+++ libnode/PriorityQueue.js
@@ -1,76 +1,89 @@
 "use strict";
 
 Object.defineProperty(exports, "__esModule", {
-	value: true
+    value: true
 });
+/**
+ * @file 优先队列实现
+ * @author STLighter <123939775@qq.com>
+ */
+
 class PriorityQueue {
-	constructor(cmp = (a, b) => a.toString() < b.toString() ? -1 : 1) {
-		this.cmp = cmp;
-		this.queue = [];
-	}
-	get length() {
-		return this.queue.length;
-	}
-	_push(val) {
-		const { queue, cmp } = this;
-		// 要加入的元素的角标作为当前元素
-		let cur = queue.length;
-		const ret = queue.push(val);
-		// 当前元素不为根节点
-		while (cur) {
-			// 获取父节点角标, 等价于Math.floor((cur - 1) / 2)
-			const parent = (cur - (cur & 1 ? 1 : 2)) / 2;
-			// 当前元素比父元素大, 小顶堆恢复
-			if (cmp(queue[parent], queue[cur]) <= 0) break;
-			// 否则继续交换直到恢复小顶堆
-			[queue[cur], queue[parent]] = [queue[parent], queue[cur]];
-			cur = parent;
-		}
-		return ret;
-	}
-	front() {
-		return this.queue[0];
-	}
-	push(...args) {
-		args.forEach(val => this._push(val));
-	}
-	pop() {
-		const { queue, cmp } = this;
-		// 顶部元素与最后一个元素交换
-		const last = queue.length - 1;
-		if (last < 1) return queue.pop();
-		[queue[0], queue[last]] = [queue[last], queue[0]];
+    constructor(cmp) {
+        this.cmp = cmp;
+        this.queue = [];
+    }
+    get length() {
+        return this.queue.length;
+    }
+    _push(val) {
+        const { queue, cmp } = this;
+        // 要加入的元素的角标作为当前元素
+        let cur = queue.length;
+        const ret = queue.push(val);
+        // 当前元素不为根节点
+        while (cur) {
+            // 获取父节点角标, 等价于Math.floor((cur - 1) / 2)
+            const parent = (cur - (cur & 1 ? 1 : 2)) / 2;
+            // 当前元素比父元素大, 小顶堆恢复
+            if (cmp(queue[parent], queue[cur]) <= 0) {
+                break;
+            }
+            // 否则继续交换直到恢复小顶堆
+            [queue[cur], queue[parent]] = [queue[parent], queue[cur]];
+            cur = parent;
+        }
+        return ret;
+    }
+    front() {
+        return this.queue[0];
+    }
+    push(...args) {
+        args.forEach(val => this._push(val));
+    }
+    pop() {
+        const { queue, cmp } = this;
+        // 顶部元素与最后一个元素交换
+        const last = queue.length - 1;
+        if (last < 1) {
+            return queue.pop();
+        }
+        [queue[0], queue[last]] = [queue[last], queue[0]];
 
-		let cur = 0,
-		    left = 2 * cur + 1,
-		    right = left + 1;
-		// 当前元素还有儿子(即当前元素不是叶子节点)
-		while (left < last) {
-			// 取较小的一个儿子的角标
-			let target = left;
-			if (right < last && cmp(queue[right], queue[left]) < 0) target = right;
+        let cur = 0;
+        let left = 2 * cur + 1;
+        let right = left + 1;
+        // 当前元素还有儿子(即当前元素不是叶子节点)
+        while (left < last) {
+            // 取较小的一个儿子的角标
+            let target = left;
+            if (right < last && cmp(queue[right], queue[left]) < 0) {
+                target = right;
+            }
 
-			// 当前元素比子元素小, 小顶堆恢复
-			if (cmp(queue[cur], queue[target]) <= 0) break;
-			// 否则继续交换
-			[queue[cur], queue[target]] = [queue[target], queue[cur]];
-			cur = target;
-			left = 2 * cur + 1;
-			right = left + 1;
-		}
-		return queue.pop();
-	}
-	// 此操作用处不大
-	// popAll () {
-	// 	const ret = [];
-	// 	while(this.length) {
-	// 		ret.push(this.pop());
-	// 	}
-	// 	return ret;
-	// }
-	// 由于操作会影响内部存储, 移除迭代器特性
-	// *[Symbol.iterator] () {
-	// 	while(this.length) yield this.pop();
-	// }
+            // 当前元素比子元素小, 小顶堆恢复
+            if (cmp(queue[cur], queue[target]) <= 0) {
+                break;
+            }
+            // 否则继续交换
+            [queue[cur], queue[target]] = [queue[target], queue[cur]];
+            cur = target;
+            left = 2 * cur + 1;
+            right = left + 1;
+        }
+        return queue.pop();
+    }
+    // 此操作用处不大
+    // popAll () {
+    //     const ret = [];
+    //     while(this.length) {
+    //         ret.push(this.pop());
+    //     }
+    //     return ret;
+    // }
+    // 由于操作会影响内部存储, 移除迭代器特性
+    // *[Symbol.iterator] () {
+    //     while(this.length) yield this.pop();
+    // }
 }
 exports.default = PriorityQueue;
--- libnode/index.js
+++ libnode/index.js
@@ -12,11 +12,30 @@ var _asyncGenerator2 = require('babel-runtime/helpers/asyncGenerator');
 
 var _asyncGenerator3 = _interopRequireDefault(_asyncGenerator2);
 
+var _lodash = require('lodash.groupby');
+
+var _lodash2 = _interopRequireDefault(_lodash);
+
 var _PriorityQueue = require('./PriorityQueue');
 
 var _PriorityQueue2 = _interopRequireDefault(_PriorityQueue);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+/**
+ * 合并多个异步迭代器，返回一个新的异步迭代器
+ * 该迭代器每次返回 pickNumber 个数据
+ * 数据按照 sortFn 排序
+ *
+ * @param {Array} iterators 异步迭代器数组对象
+ * @param {Function} sortFn 对请求结果进行排序的函数
+ * @param {number} pickNumber 迭代器每次返回的元素数量
+ */
+/**
+ * @file MixLoader
+ * @author LeuisKen <leuisken@foxmail.com>
+ *         STLighter <123939775@qq.com>
+ */
 
 exports.default = (() => {
     var _ref = _asyncGenerator3.default.wrap(function* (iterators, sortFn, pickNumber) {
@@ -40,41 +59,26 @@ exports.default = (() => {
 
         // 还有数据可以返回
         while (dataSources.length > 0 || dataQueue.length) {
-            // 所有待消费数据余量为0的数据源
-            const drySrc = dataSources.filter(function (src) {
-                return !src.poolSize;
-            });
-            // 这些数据源需使用迭代器获取一次数据, 并需要等待返回
+            // 数据源分组
+            const { drySrc = [], dryingSrc = [] } = (0, _lodash2.default)(dataSources, sourceSeparator);
+            // 对于余量为 0 的数据源
+            // 策略为获取一次数据, 并需要等待返回
             const reqs = drySrc.map(function (src) {
                 return src.req || src.iterator.next();
             });
-
-            // 所有待消费数据余量不足pickNumber的数据源
-            const dryingSrc = dataSources.filter(function (src) {
-                return src.poolSize > 0 && src.poolSize < pickNumber && !src.req;
-            });
-            // 这些数据源需使用迭代器预先获取一次数据, 但不需要等待返回
+            // 对于余量大于 0 小于 pickNumber 的数据源
+            // 策略为预先获取一次数据, 但不需要等待返回
             dryingSrc.forEach(function (src) {
                 return src.req = src.iterator.next();
             });
 
-            // 等待所有余量为0的数据源返回, 保证每个数据源都有数据在优先队列中
+            // 等待所有余量为 0 的数据源返回，保证每个数据源都有数据在优先队列中
             const res = yield _asyncGenerator3.default.await(_promise2.default.all(reqs));
             for (let i = 0; i < res.length; i++) {
-                // 处理迭代器返回的数据
-                drySrc[i].req = null;
-                const { value, done } = res[i];
+                // 根据返回结果更新数据队列与数据源列表
                 // 如果该迭代器未迭代完, 将数据放入优先队列中
-                if (!done) {
-                    dataQueue.push(...value.map(function (value) {
-                        return { value, src: drySrc[i] };
-                    }));
-                    drySrc[i].poolSize += value.length;
-                }
                 // 否则, 将数据源从列表中移除
-                else {
-                        dataSources.splice(dataSources.indexOf(drySrc[i]), 1);
-                    }
+                updateData(drySrc[i], res[i]);
             }
 
             // 使用优先队列将数据按序放入结果集
@@ -82,30 +86,60 @@ exports.default = (() => {
                 // 将最优先的一条数据从队列移除, 并放入结果集中
                 const item = dataQueue.pop();
                 dataSet.push(item.value);
-                // 对应数据源未消费数据减少1
+                // 对应数据源未消费数据减少 1
                 --item.src.poolSize;
-                // 如果此时数据源待消费数据量为0, 且结果集数据不足, 需要等待此数据源迭代器返回新数据
+                // 如果此时数据源待消费数据量为 0 , 且结果集数据不足, 需要等待此数据源迭代器返回新数据
                 if (!item.src.poolSize && dataSet.length < pickNumber) {
-                    // 由于目前的与获取策略, 只会走item.src.req, 后者用于兼容不同的预获取策略
+                    // 由于目前的预获取策略, 只会走 item.src.req , 后者用于兼容不同的预获取策略
                     item.src.req = item.src.req || item.src.iterator.next();
-                    const { value, done } = yield _asyncGenerator3.default.await(item.src.req);
-                    item.src.req = null;
-                    // 如果该迭代器未迭代完, 将数据放入优先队列中
-                    if (!done) {
-                        dataQueue.push(...value.map(function (value) {
-                            return { value, src: item.src };
-                        }));
-                        item.src.poolSize += value.length;
-                    }
-                    // 否则, 将数据源从列表中移除
-                    else {
-                            dataSources.splice(dataSources.indexOf(item.src), 1);
-                        }
+                    const result = yield _asyncGenerator3.default.await(item.src.req);
+                    updateData(item.src, result);
                 }
             }
 
             // 从结果中取出 pickNumber 个数据
             yield dataSet.splice(0, pickNumber);
+        }
+
+        /**
+         * 根据待消费数据余量，对数据源进行分组
+         *
+         * @param {Object} source 数据源
+         * @return {string} 分组的 key 名
+         */
+        function sourceSeparator(source) {
+            // 所有待消费数据余量为 0 的数据源
+            if (source.poolSize === 0) {
+                return 'drySrc';
+            }
+            // 所有待消费数据余量不足 pickNumber 的数据源
+            else if (source.poolSize < pickNumber && !source.req) {
+                    return 'dryingSrc';
+                }
+            return 'other';
+        }
+
+        /**
+         * 根据迭代器的返回数据，更新数据队列与数据源列表
+         *
+         * @param {Object} source 迭代器对应的数据源
+         * @param {Object} result 迭代器的返回值
+         */
+        function updateData(source, result) {
+            const { value, done } = result;
+            source.req = null;
+            // 如果该迭代器未迭代完, 将数据放入优先队列中
+            if (!done) {
+                dataQueue.push(...value.map(value => ({
+                    value,
+                    src: source
+                })));
+                source.poolSize += value.length;
+            }
+            // 否则, 将数据源从列表中移除
+            else {
+                    dataSources.splice(dataSources.indexOf(source), 1);
+                }
         }
     });
 
@@ -114,17 +148,4 @@ exports.default = (() => {
     }
 
     return mixLoader;
-})(); /**
-       * @file MixLoader
-       * @author LeuisKen <leuisken@foxmail.com>
-       */
-
-/**
- * 合并多个异步迭代器，返回一个新的异步迭代器
- * 该迭代器每次返回 pickNumber 个数据
- * 数据按照 sortFn 排序
- *
- * @param {Array} iterators 异步迭代器数组对象
- * @param {Function} sortFn 对请求结果进行排序的函数
- * @param {number} pickNumber 迭代器每次返回的元素数量
- */
+})();
